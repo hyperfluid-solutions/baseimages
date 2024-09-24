60fd7d28ce958a9651cbf5231ba1fb7765c8c1a4
Jonathan Shaffer <jon@hyperfluidsolutions.com>
2024-09-23 21:08:39 -0400

    ci: adding remotes for simple version selection on blogdown

--- blog/src/install.R
+++ blog/src/install.R
@@ -1,6 +1,11 @@
 #!/usr/bin/env Rscript
 
+install.package("remotes")
+
 require(remotes)
 # Blogdown versions:
 # https://github.com/rstudio/blogdown/releases
 install_version("blogdown", "1.19" repos = "http://cran.us.r-project.org")
+
+require(blogdown)
+install_hugo(version: "latest", extended: TRUE)
